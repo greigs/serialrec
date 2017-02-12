2320702bfe40ed02cffe20aef79ceea651ed01a4
greig <gstewart@streamba.net>
2017-02-12 14:17:33 +0000

    testing serial com

---
+++ pcside/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static FileStream fileStream;
+        static BinaryWriter bw;
+        private static bool first = true;
+
+        private static SerialPort sp;
+        static void Main(string[] args)
+        {
+
+            int buffsize = 16;
+            var f = File.Open("c:\\temp\\big_buck_bunny_480p_surround-fix.avi",FileMode.Open);
+
+            sp = new SerialPort("COM6", 9600)
+            {
+                //
+                //ReadBufferSize = 2000,
+                //DiscardNull = true,Handshake = Handshake.None,
+                //DtrEnable = false,
+                Handshake = Handshake.None,
+                //RtsEnable = false,
+                //DiscardNull = false,
+                ReadBufferSize = 5000,
+                WriteBufferSize = 5000,
+                //Handshake = Handshake.XOnXOff,
+                //Parity = Parity.Even,
+                DataBits = 8,
+                StopBits = StopBits.One,
+                ReceivedBytesThreshold = 3000,
+                DtrEnable = false,
+                RtsEnable = false,
+                
+                Encoding = System.Text.Encoding.GetEncoding("iso-8859-1"),
+            };
+            
+            File.Delete("c:\\temp\\out.avi");
+            
+            fileStream = File.Create("c:\\temp\\out.avi");
+            bw = new BinaryWriter(fileStream,Encoding.GetEncoding("iso-8859-1"));
+
+            Console.WriteLine("Press any key to continue (1)...");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            sp.Open();
+            sp.DiscardInBuffer();
+
+
+            sp.ErrorReceived += SpOnErrorReceived;
+            //sp.DataReceived += Sp_DataReceived;
+
+            Console.WriteLine("Press any key to continue (2)...");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            
+            Console.WriteLine("Running");
+
+            //remove byte at 0x3328, 13096
+            //int bufferSize = int.Parse("4000", System.Globalization.NumberStyles.HexNumber);
+
+            long bytesWritten = 0;
+            byte[] buff = new byte[16];
+            while (true)
+            {
+                var bytesToRead = sp.BytesToRead;
+                if (bytesToRead > 0)
+                {
+
+                    var buffer = new byte[bytesToRead];
+
+                    sp.Read(buffer,0, bytesToRead);
+
+                    
+
+                    f.Seek(bytesWritten, SeekOrigin.Begin);
+                    
+                    f.Read(buff, 0, buffsize);
+
+                    bool match = MatchesFirstBytes(buffsize, buff, buffer);
+
+                    if (match)
+                    {
+                        Console.WriteLine(bytesToRead + "OK");
+                    }
+                    else
+                    {
+                        Console.WriteLine(bytesToRead + "ERROR");
+                    }
+
+                    //bw.Write(buffer);
+
+                    bytesWritten += bytesToRead;
+
+                }
+            }
+
+        }
+
+        private static bool MatchesFirstBytes(int buffsize, byte[] buff, byte[] buffer)
+        {
+            bool match = true;
+            for (var i = 0; i < buffsize; i++)
+            {
+                if (buff[i] != buffer[i])
+                {
+                    match = false;
+                }
+            }
+            return match;
+        }
+
+        //private static void Sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        //{
+
+        //    var bytesToRead = sp.BytesToRead;
+        //    if (bytesToRead == 3000)
+        //    {
+                
+        //    }
+
+        //    if (bytesToRead > 0)
+        //    {
+        //       // Console.WriteLine(bytesToRead);
+        //        byte[] buffer = new byte[bytesToRead];
+        //        sp.Read(buffer, 0, bytesToRead);
+        //        bw.Write(buffer);
+        //    }
+        //    //Thread t = new Thread(Go);
+        //    //t.Start();
+
+
+        //}
+
+        private static void Go()
+        {
+
+        }
+
+        private static void SpOnErrorReceived(object sender, SerialErrorReceivedEventArgs serialErrorReceivedEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ pcside/SerialSender/SerialSender/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SerialSender
+{
+    class Program
+    {
+
+
+        static int bufferSize = 2048;
+        static double delayms = 0.001;
+
+        static void Main()
+        {
+
+            Thread t = new Thread(UpdateSettings);
+            t.Start();
+
+            bool writeToSerial = true;
+            bool writeToFile = false;
+            var serialPortName = "COM6";
+            var serial = new SerialPort(serialPortName, 9600)
+            {
+                //
+                //ReadBufferSize = 2000,
+                //DiscardNull = true,Handshake = Handshake.None,
+                //DtrEnable = false,
+                Handshake = Handshake.None,
+                //RtsEnable = false,
+                //DiscardNull = false,
+                ReadBufferSize = 5000,
+                WriteBufferSize = 5000,
+                //Handshake = Handshake.XOnXOff,
+                //Parity = Parity.Even,
+                DataBits = 8,
+                StopBits = StopBits.One,
+                ReceivedBytesThreshold = 3000,
+                DtrEnable = false,
+                RtsEnable = false,
+            };
+
+            if (writeToSerial)
+            {
+                Console.WriteLine("Opening Serial");
+                serial.Open();
+                Console.WriteLine("Serial Open");
+            }
+
+            Stream inStream = GetStandardInputStream();
+            FileStream outFileStream = null;
+            if (writeToFile)
+            {
+                outFileStream = File.Create("out.avi");
+            }
+            bool first = true;
+            
+            while (true)
+            {
+                byte[] buffer = new byte[bufferSize];
+
+                inStream.Read(buffer, 0, buffer.Length);
+                Thread.Sleep(TimeSpan.FromMilliseconds(delayms));
+                if (writeToSerial)
+                {
+
+                    if (first)
+                    {
+                        first = false;
+                        var str = System.Text.Encoding.UTF8.GetString(buffer);
+                        var valid = str.StartsWith("#!rtpplay1.0 127.0.0.1");
+                        Console.WriteLine(valid ? "ok" : "bad");
+                    }
+                    //Console.WriteLine("Streaming " + bufferSize + " bytes");
+                    serial.Write(buffer, 0, buffer.Length);
+                    //Thread.Sleep(10);
+                    //Console.WriteLine("Finished streaming " + bufferSize + " bytes");
+                }
+                if (writeToFile)
+                {
+                    outFileStream.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+
+        private static void UpdateSettings()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                var lines = File.ReadLines("settings.txt").ToArray();
+                var buffer = int.Parse(lines[0]);
+                bufferSize = buffer;
+                double delay = double.Parse(lines[1]);
+                delayms = delay;
+            }
+        }
+
+        private static Stream GetStandardInputStream()
+        {
+
+            return Console.OpenStandardInput(bufferSize);
+            //return File.OpenRead("C:\\repo\\rtptools\\rtptools-1.21\\Debug\\out.avi");
+        }
+
+        //private static Stream GetSerialInputStream()
+        //{
+            
+        //}
+    }
+}
