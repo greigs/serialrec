165a609adbf90a7de2092efce1e59b056ca1c63b
greig <gstewart@streamba.net>
2017-05-09 22:36:34 +0100

    added tests

--- pcside/SerialSender/SerialSender/Program.cs
+++ pcside/SerialSender/SerialSender/Program.cs
@@ -28,22 +28,18 @@ namespace SerialSender
     ///     Update: medium length messages seem to remain in order still get truncated and lost in a
     ///     buffer somewhere if too much is sent at once. Waiting and then
     ///     sending single bytes seems to push the message through,
-    ///     so keep sending these with delays inbetween to
+    ///     so keep sending thesxe with delays inbetween to
     ///     "flush" the data flow
     /// 
     ///     around 4k seems to be the magic number, then send an %IGNORE% message
-    ///     until a CRC response is returned
-    /// 
-    /// 
-    /// 
-    ///     
+    ///     with delays in between until a CRC response is returned
     /// 
     /// </summary>
-    class Program
+    public class Program
     {
-        static int bufferSize = 20;
+        static int bufferSize = 1024;
         //static int hwBufferSize = 2048;
-        private static int crcByteSize = 8;
+        private static int crcByteSize = 11;
         static double delayms = 0.001;
         private const string ReadyString = "%READY%";
         private static string clearHwBufferString;
@@ -70,7 +66,7 @@ namespace SerialSender
             }
 
 
-            var serial = new SerialPort(serialPortName, 9600)
+            var serial = new SerialPort(serialPortName, 115200)
             {
                 //
                 //ReadBufferSize = 2000,
@@ -99,27 +95,31 @@ namespace SerialSender
 
                 serial.Open();
                 Console.WriteLine("Clearing write HW buffer");
-                byte[] buffertmp = new byte[4095];
-                byte[] buffertmp2 = new byte[buffertmp.Length + 1];
+                //byte[] buffertmp = new byte[4095];
+                //byte[] buffertmp2 = new byte[buffertmp.Length + 1];
 
-                for (int i = 0; i < 1; i++)
+                //for (int i = 0; i < 1; i++)
+                //{
+                //    inStream.Read(buffertmp, 0, buffertmp.Length);
+
+                //    buffertmp.CopyTo(buffertmp2,0);
+                //    buffertmp2[buffertmp2.Length - 1] = (byte) '\n';
+                //    serial.Write(buffertmp2, 0, buffertmp2.Length);
+                //    //Thread.Sleep(TimeSpan.FromMilliseconds(delayms));
+                //}
+                for (int i = 0; i < 10; i++)
                 {
-                    inStream.Read(buffertmp, 0, buffertmp.Length);
-
-                    buffertmp.CopyTo(buffertmp2,0);
-                    buffertmp2[buffertmp2.Length - 1] = (byte) '\n';
-                    serial.Write(buffertmp2, 0, buffertmp2.Length);
-                    //Thread.Sleep(TimeSpan.FromMilliseconds(delayms));
+                    Thread.Sleep(3);
+                    serial.Write("%IGNORE%");
                 }
-
 
                 Console.WriteLine("Serial Open");
 
-                for (int i = 0; i < 15; i++)
-                {
-                    Thread.Sleep(10);
-                    serial.Write("%IGNORE%");
-                }
+                //for (int i = 0; i < 15; i++)
+                //{
+                //    Thread.Sleep(10);
+                //    serial.Write("%IGNORE%");
+                //}
             }
 
            
@@ -129,20 +129,52 @@ namespace SerialSender
                 outFileStream = File.Create("out.avi");
             }
             bool first = true;
-            
+
+            int set = 0;
             while (true)
             {
+                Console.WriteLine();
+                Console.WriteLine("Set " + set);
+                set++;
                 byte[] standardInputBuffer = new byte[bufferSize];
 
+                if (first)
+                {
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Thread.Sleep(3);
+                        serial.Write("%IGNORE%");
+                    }
+                }
+
                 inStream.Read(standardInputBuffer, 0, bufferSize - crcByteSize);
-                var crc = CalculateCRC(standardInputBuffer, bufferSize - crcByteSize);
+                string str;
+                if (first)
+                {
+                    str = System.Text.Encoding.UTF8.GetString(standardInputBuffer).Substring(13);
+                    first = false;
+                }
+                else
+                {
+                    str = System.Text.Encoding.UTF8.GetString(standardInputBuffer);
+                }
+                var len = str.Length - 11;
+                var bytes = System.Text.Encoding.UTF8.GetBytes(str);
+
+                var crc = CalculateCRC(bytes, len);
                 AddCrcToEndOfBuffer(standardInputBuffer, crc);
 
-                Thread.Sleep(TimeSpan.FromMilliseconds(delayms));
+                //Thread.Sleep(TimeSpan.FromMilliseconds(delayms));
                 if (writeToSerial)
                 {
                     // wait until we have the all clear
-                    ReadUntil(serial, ReadyString);
+                    //ReadUntil(serial, ReadyString);
+                    Console.WriteLine("Waiting for READY");
+                    ReadUntilWhileSendingIgnore(serial, "%READY%");
+                    Console.WriteLine("Got READY");
+
+
                     //if (first)
                     //{
                     //    first = false;
@@ -151,7 +183,7 @@ namespace SerialSender
                     //    Console.WriteLine(valid ? "ok" : "bad");
                     //}
                     WriteBytesToSerialWithRetry(serial, standardInputBuffer, crc);
-                    Console.WriteLine("Response ok, waiting for next ready signal to continue");
+                    //Console.WriteLine("Response ok, waiting for next ready signal to continue");
                 }
                 if (writeToFile)
                 {
@@ -169,37 +201,249 @@ namespace SerialSender
 
         private static void WriteBytesToSerialWithRetry(SerialPort serial, byte[] standardInputBuffer, string crc)
         {
-            Console.WriteLine(Encoding.ASCII.GetString(standardInputBuffer));
+            //Console.WriteLine(Encoding.ASCII.GetString(standardInputBuffer));
             serial.Write(standardInputBuffer, 0, standardInputBuffer.Length);
             while (serial.BytesToWrite > 0)
             {
                 Thread.Sleep(1);
             }
-            Console.WriteLine("read bytes wirh crc " + crc);
+            Console.WriteLine("Sent data, waiting for CRC response (sending ignores)");
             // await response
-            var response = ReadCharsAsASCII(serial, "7c5f0cc8 CRC OK%%".Length);
-            while (!ResponseIsOk(response, crc))
+
+            var response = SendIgnoreUntilResponse(serial, "7c5f0cc8 CRC OK%%%".Length);
+            while (!ResponseIsOk("CRC" + response, crc))
             {
-                Console.WriteLine("Response NOT ok, waiting for ready signal to retry");
-                ReadUntil(serial, ReadyString);
-                Console.WriteLine(Encoding.ASCII.GetString(standardInputBuffer));
+                Console.WriteLine($"Response NOT ok ({response}), waiting for ready signal to retry");
+                ReadUntil(serial, ReadyString, response);
+                Console.WriteLine("Got READY, resending");
+                //Console.WriteLine(Encoding.ASCII.GetString(standardInputBuffer));
                 serial.Write(standardInputBuffer, 0, standardInputBuffer.Length);
+                Thread.Sleep(1000);
                 while (serial.BytesToWrite > 0)
                 {
                     Thread.Sleep(1);
                 }
-                response = ReadCharsAsASCII(serial, "7c5f0cc8 CRC OK%%".Length);
+                Console.WriteLine("waiting for CRC response (sending ignores)");
+                response = SendIgnoreUntilResponse(serial, "7c5f0cc8 CRC OK%%%".Length);
             }
+
+            Console.WriteLine("Response OK");
            
+        }
+
+        private static string SendIgnoreUntilResponse(SerialPort serial, int length)
+        {
+            if (serial.BytesToRead > 0)
+            {
+                var read = ReadCharsAsASCII(serial, serial.BytesToRead);
+                if (read.Length > length)
+                {
+                    return read.Substring(read.Length - length - 1, length);
+                }
+            }
+
+            bool keepsending = true;
+            var t = new Task(() =>
+            {
+                while (keepsending)
+                {
+                    for (int i = 0; i < 1; i++)
+                    {
+                        Thread.Sleep(TimeSpan.FromMilliseconds(1));
+                        serial.Write("%IGNORE%");
+                    }
+                }
+            });
+            t.Start();
+            var response = ReadCharsAsASCII(serial, length);
+
+            if (response.Contains("OK"))
+            {
+                
+            }
+
+            else if (response.Contains("CRC ERROR"))
+            {
+
+            }
+            else
+            {
+                ;
+            }
+
+
+
+            keepsending = false;
+            t.Wait();
+            return response;
         }
 
         private static bool ResponseIsOk(string response, string crc)
         {
-            return response.StartsWith(crc) && response.Contains("CRC OK%%");
+            if (response != "CRC00000000 CRC ERROR")
+            {
+                
+            }
+
+            return response.StartsWith(crc) && response.Contains("CRC OK!!!");
         }
 
-        private static void ReadUntil(SerialPort serial, string matchString)
+        private static void ReadUntil(SerialPort serial, string matchString, string alreadyRead = null)
         {
+
+            var matchOnResult = matchString;
+
+            if (alreadyRead != null)
+            {
+                matchOnResult = CalculateRequiredMatch(matchString, alreadyRead);
+            }
+            
+
+            var allMatch = false;
+            int charIndex = 0;
+            while (!allMatch)
+            {
+
+                var data = (char) serial.ReadChar();
+                if (data == matchOnResult[charIndex])
+                {
+                    charIndex++;
+                }
+                else
+                {
+                    charIndex = 0;
+                }
+
+                if (charIndex == matchString.Length)
+                {
+                    allMatch = true;
+                    //Console.WriteLine("got %READY%");
+                }
+            }
+            
+
+        }
+
+        /// <summary>
+        /// Calculates how much of a string is still needed to be read given the matching partial or whole string.
+        /// Returns null if whole string matches
+        /// </summary>
+        /// <param name="matchString"></param>
+        /// <param name="alreadyRead"></param>
+        /// <returns></returns>
+        public static string CalculateRequiredMatch(string matchString, string alreadyRead)
+        {
+            string matchOn = matchString;
+
+            // read back the "alreadyRead" string, looking for a full, then partial match
+
+            if (alreadyRead.EndsWith(matchString))
+            {
+                // done, full match, no need to read more
+                return null;
+            }
+
+            var lastChar = alreadyRead.Last();
+            if (matchString.Contains(lastChar))
+            {
+                // find the position of the last char from the alreadyread in the match string
+                var indexWithinMatchString = matchString.LastIndexOf(lastChar);
+
+                // match the rest of the string back fowards the start,
+                // make sure to not go below the first char
+
+                if (indexWithinMatchString == (matchString.Length - 1))
+                {
+                    // the last character is a match, but the endswith check failed
+                    // therefore must be a partial match containing the
+                    // same matched character somewhere earlier in the matchstring
+                    // need to find the entry point of this substring and match the rest
+
+                    // note the previous character - the second last character that matched in alreadyread
+                    var indexOfPrevCharAfterMatched = alreadyRead.Length - matchString.Length + indexWithinMatchString - 1;
+                    var prevChar = alreadyRead[indexOfPrevCharAfterMatched];
+                    int matchedIndex = -1;
+                    for (var i = indexWithinMatchString - 1; i >= 0; i--)
+                    {
+                        if (prevChar == matchString[i])
+                        {
+                            matchedIndex = i;
+                            break;
+                        }
+                    }
+
+                    if (matchedIndex > -1)
+                    {
+                        if (matchedIndex == 0)
+                        {
+                            //TODO
+                        }
+                        else
+                        {
+                            bool ok = true;
+                            for (var i = matchedIndex - 1; i >= 0; i--)
+                            {
+                                var ch = alreadyRead[alreadyRead.Length - matchString.Length - matchedIndex + i - 1 + indexWithinMatchString];
+                                if (ch != matchString[i])
+                                {
+                                    ok = false;
+                                    break;
+                                }
+                            }
+                            if (ok)
+                            {
+                                return matchString.Substring(matchedIndex + 2);
+                            }
+                        }                        
+                    }
+                }
+                else if (indexWithinMatchString == 0) 
+                {
+                    // should not happen (this would be a full match)?
+                    throw new Exception();
+                }
+                else // didn't match on the last character of matchedstring
+                {
+                    var matchedLength = 0;
+                    for (var i = indexWithinMatchString; i >= 0; i--)
+                    {
+                        var ch = alreadyRead[alreadyRead.Length - (indexWithinMatchString - i) - 1];
+                        if (matchString[i] == ch)
+                        {
+                            matchedLength++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                    }
+
+                    return matchString.Substring(matchedLength);
+
+                }
+            }
+
+            return matchOn;
+        }
+
+        private static void ReadUntilWhileSendingIgnore(SerialPort serial, string matchString)
+        {
+
+            bool keepsending = true;
+            var t = new Task(() =>
+            {
+                while (keepsending)
+                {
+                    for (int i = 0; i < 1; i++)
+                    {
+                        Thread.Sleep(TimeSpan.FromMilliseconds(1));
+                        serial.Write("%IGNORE%");
+                    }
+                }
+            });
+            t.Start();
+
             var allMatch = false;
             int charIndex = 0;
             while (!allMatch)
@@ -218,7 +462,10 @@ namespace SerialSender
                 if (charIndex == matchString.Length)
                 {
                     allMatch = true;
-                    Console.WriteLine("got %READY%");
+                    keepsending = false;
+                    t.Wait();
+
+                    //Console.WriteLine("got %READY%");
                 }
             }
         }
@@ -236,15 +483,15 @@ namespace SerialSender
 
         private static string CalculateCRC(byte[] data, int length)
         {
-            Crc32 crc32 = new Crc32();
-            string hash = string.Empty;
+            var crc32 = new Crc32();
+            var hash = string.Empty;
 
             int bytecount = 0;
-            foreach (byte b in crc32.ComputeHash(data))
+            foreach (var b in crc32.ComputeHash(data,0,length))
             {
                 if (bytecount < length)
                 {
-                    hash += b.ToString("x2").ToLower();
+                    hash += b.ToString("x2").ToUpper();
                     bytecount++;
                 }
                 else
@@ -253,7 +500,7 @@ namespace SerialSender
                 }
             };
 
-            return hash;
+            return  "CRC" + hash;
         }
 
         private static void UpdateSettings()
---
+++ pcside/SerialSender/SerialSenderTests/ProgramTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SerialSender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialSender.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void CalculateRequiredMatchFullString()
+        {
+            Assert.IsNull(Program.CalculateRequiredMatch("%READY%", "sjhdfjhasgdfhjas%READY%"));
+        }
+
+        [TestMethod]
+        public void CalculateRequiredMatchIfPartialMatch()
+        {
+            var result = Program.CalculateRequiredMatch("%READY%", "sdhgfjg%REA");
+            Assert.AreEqual("DY%", result);
+        }
+
+        [TestMethod]
+        public void CalculateRequiredMatchIfPartialMatchAtEndWithDuplicateChars()
+        {
+            var result = Program.CalculateRequiredMatch("%RE%ADY%", "sdfgdsfgsfsdhgf%RE%");
+            Assert.AreEqual("ADY%", result);
+
+            var result2 = Program.CalculateRequiredMatch("%REDO123%ADY456%", "sdfgdsfgsfsdfsdhgf%REDO123%");
+            Assert.AreEqual("ADY456%", result2);
+
+        }
+
+        [TestMethod]
+        public void CalculateRequiredMatchIfPartialMatch2()
+        {
+            var result = Program.CalculateRequiredMatch("%READY%", "sdfgdsfgsfsdhg%R");
+            Assert.AreEqual("EADY%", result);
+
+            var result2 = Program.CalculateRequiredMatch("%READY%", "sdfgdsfgsfsdhg%RE");
+            Assert.AreEqual("ADY%", result2);
+
+        }
+
+
+    }
+}
